41593f3527c90e3578e4e093a7fd4fb2bc863d15
nurlan985 <nurlan985@gmail.com>
2020-03-16 23:16:45 -0500

    test commit

--- StudentEnrollment.Domain/src/main/java/edu/miu/cs/cs544/domain/Student.java
+++ StudentEnrollment.Domain/src/main/java/edu/miu/cs/cs544/domain/Student.java
@@ -1,5 +1,28 @@
 package edu.miu.cs.cs544.domain;
 
-public class Student extends Person {
+import java.util.Set;
 
+import javax.persistence.Entity;
+
+import edu.miu.cs.cs544.domain.security.Role;
+
+@Entity
+public class Student extends Person {
+	private String studentId;
+
+	public Student() { }
+
+	public Student(String name, String email, String password, Set<Role> roles, String studentId) {
+		super(name, email, password, roles);
+		this.studentId = studentId;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	
 }
