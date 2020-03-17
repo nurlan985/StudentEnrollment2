234be37c0b115f8d2d966f26c44bb150b89bbe7b
James Igaba <jamesigaba@gmail.com>
2020-03-17 14:59:48 -0500

    student CRUD

---
+++ StudentEnrollment.Web/src/main/java/edu/miu/cs/cs544/group1/controller/StudentController.java
@@ -0,0 +1,23 @@
+package edu.miu.cs.cs544.group1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.miu.cs.cs544.group1.domain.security.Student;
+import edu.miu.cs.cs544.group1.service.StudentService;
+
+@RestController
+@EnableAutoConfiguration
+public class StudentController {
+	
+	@Autowired
+	private StudentService studentService;
+	
+	@PostMapping("/addStudent")
+	public Student addStudent(Student student) {
+		return studentService.addStudent(student);
+	}
+
+}
